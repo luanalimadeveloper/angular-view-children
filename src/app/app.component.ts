32899ee05f8b81d9480e111d5c08d1f40bbdca47
Luana Lima <luanalima.developer@gmail.com>
2024-04-07 17:52:15 -0300

    ViewChildren() in Angular | Angular Component & Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChildren, QueryList } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-view-children';
+  fullName: string = '';
+  @ViewChildren('inputEl', {}) inputElements: QueryList<ElementRef>;
+
+  show(){
+    let name = ''
+    this.inputElements.forEach((el) => {
+      name += el.nativeElement.value + ' '
+    })
+    this.fullName = name.trim();
+  }
 }
